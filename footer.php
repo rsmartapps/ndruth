44273d1e91b9000d30e8e3e171e9b48b98a974ff
unknown <rubensa87@hotmail.es>
2020-05-25 10:07:53 +0200

    add git url in styles file

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="footer">
        <hr />
         <div class="sixteen columns">
-            <?php if ( is_active_sidebar( 'footer-widget' ) ) : ?> <?php dynamic_sidebar( 'footer-widget' ); ?>
+            <?php if ( is_active_sidebar( 'footer-widget' ) ) : ?> <?php dynamic_sidebar( 'footer-widgetgit s' ); ?>
             <?php else : ?><p>You need to drag a widget into your sidebar in the WordPress Admin</p>
 	        <?php endif; ?> 
         </div>
