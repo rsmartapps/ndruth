09ce950d2fb1bdebf0dd12ed02c97eaf518aab34
unknown <rubensa87@hotmail.es>
2020-05-25 21:30:18 +0200

    modify widget

--- js/widget-images.js
+++ js/widget-images.js
@@ -1,5 +1,13 @@
 jQuery(function($){
-    $(document).ready(function($){
-        console.log("todo ok");
+    $('.card.h-100.shadow > img').each(function(i) {
+        img = jQuery(this);
+        img.removeClass();
+        img.addClass('card-img-top mh-100');
+        img.removeAttr('width');
+        img.removeAttr('height');
+        img.removeAttr('style');
+        img.removeAttr('sizes');
+        img.attr('preserveaspectratio','xMidYMid slice');
+        img.wrap( '<div class="d-flex align-items-center" style="height:250px"></div>' );
     });
 });
