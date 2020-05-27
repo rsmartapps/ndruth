881c8b3b4c4f793b17d840d76a7ddc8c745897ab
unknown <rubensa87@hotmail.es>
2020-05-27 19:36:59 +0200

    modify logo url

--- inc/templates/views/html-site-logo.php
+++ inc/templates/views/html-site-logo.php
@@ -5,7 +5,7 @@
 ?>
 <!-- Menu section -->
 
-  <a class="navbar-brand" href="/">
+  <a class="navbar-brand" href="<?php echo esc_url(get_template_directory_uri()); ?>">
       <!-- <img src="http://localhost:8081/public/images/logo-en.png" class="d-inline-block align-top"> -->
       <div class="brand d-flex align-items-center font-weight-bold" ><div class="text-dark" style="padding-right:8px;font-size:49px;line-height:1;">ND</div><div class="d-flex flex-column text-primary font-weight-bolder">Ruth Sebastian<span class="font-weight-normal">Nutrition and Dietetics</span></div></div>
   </a>
